d9e1dfbf7a83e4c47ec9ffc4b9acb11a3cc89888
claire abadji <claire.abadji@gmail.com>
2020-05-12 16:35:48 +0200

    listing articles

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require "faker"
+# TODO: Write a seed to insert 10 articles in the database
+
+puts 'Creating 10 fake articles...'
+10.times do
+  article = Article.create!(title: Faker::TvShows::BreakingBad.character, content: Faker::TvShows::BreakingBad.episode)
+end
+puts 'Finished!'
+
+
+# fake url Faker::Internet.url
+
